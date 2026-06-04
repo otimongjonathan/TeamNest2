2350a98b509e094a3133d55bfdd5335821b0916c
Mustaf10 <xaviermustaf10@gmail.com>
2026-06-04 12:36:28 +0300

    remove unused imports in Theme.kt

--- app/src/main/java/com/example/teamnest/ui/theme/Theme.kt
+++ app/src/main/java/com/example/teamnest/ui/theme/Theme.kt
@@ -1,15 +1,12 @@
 package com.example.teamnest.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
@@ -17,7 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
